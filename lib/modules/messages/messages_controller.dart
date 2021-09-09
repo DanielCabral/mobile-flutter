e382eb395d23a67b46abf9aa4777919d850b15a6
jecAmorim <jeciane_amorim@hotmail.com>
2021-09-08 23:58:07 -0300

    adjustment in page messages

---
+++ lib/modules/messages/messages_controller.dart
@@ -0,0 +1,7 @@
+class MessagesController {
+  var currentPage = 0;
+
+  void setPage(int index){
+    currentPage = index;
+  }
+}
--- lib/modules/messages/messages_page.dart
+++ lib/modules/messages/messages_page.dart
@@ -1,65 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:learnenglish/modules/messages/messages_controller.dart';
 import 'package:learnenglish/shared/themes/app_colors.dart';
 
-class MessagesPage extends StatelessWidget {
+class MessagesPage extends StatefulWidget {
   const MessagesPage({Key? key}) : super(key: key);
 
+  @override
+  _MessagesPageState createState() => _MessagesPageState();
+}
+
+class _MessagesPageState extends State<MessagesPage> {
+  final controller = MessagesController();
+
+  final pages = [
+    Container(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+          ],
+        ),
+      ),
+    )];
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-        backgroundColor: AppColors.background,
-        body: (Container(
-            width: size.width,
-            height: size.height,
-            child: Stack(children: [
-              Container(
-                width: size.width,
-                height: size.height * 0.25,
-                color: AppColors.primary,
-              ),
-              Positioned(
-                  top: 54,
-                  left: 0,
-                  right: 0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 24, right:24, top: 24),
-                    child: Container(
-                      child: Row(
-                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Messages", style: TextStyle(fontSize: 22, color: AppColors.background),),
-                          Icon(Icons.more_vert, color: AppColors.background)
-                        ])
-                    ),
-                  )
-              ),
-              Positioned(
-                top: size.height * 0.20,
-                left: 0,
-                right: 0,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 24, right: 24),
-                  child:Container(
-                    
-                    decoration: BoxDecoration(
-                      color: AppColors.background,
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.fromBorderSide(BorderSide(color: AppColors.grey)),
-                    ),
-                    child: TextField(
-                          //controller: nameController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Search contact',
-                            suffixIcon: Icon(Icons.search),
-                            fillColor: AppColors.error,
-                          ),
-                        ),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(152),
+          child: Container(
+              height: 142,
+              color: AppColors.primary,
+              child: Center(
+                  child: ListTile(
+                      title: Text(
+                "Aulas",
+                style: TextStyle(
+                    color: AppColors.background,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              )))),
+        ),
+        body: Container(),
+        bottomNavigationBar: Container(
+            height: 60,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/home");
+                    },
+                    icon: Icon(Icons.home_outlined, color: AppColors.textgrey),
                   ),
-                ),
-              ),
-            ]))));
+                  IconButton(
+                    onPressed: () {
+                      controller.setPage(0);
+                    },
+                    icon: Icon(Icons.description, color: AppColors.primary),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/messages");
+                    },
+                    icon: Icon(Icons.mail_outline, color: AppColors.textgrey),
+                  )
+                ])));
   }
 }
